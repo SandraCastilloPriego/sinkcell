b071d8e87e8aef16164b7a05de20d525322f2781
nolssi <nolssi@3a5c28fd-5e28-a3e6-50a2-b0e0907970cd>
2010-10-02 14:55:00 +0000

    Initial sources

---
+++ trunk/sinkJava/src/sinkjava/ImportXML.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2006-2010 The sinkCell Development Team
+ *
+ * This file is part of sinkCell.
+ *
+ * sinkCell is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * sinkCell is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * sinkCell; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package sinkjava;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ *
+ * @author sandra
+ */
+public class ImportXML extends DefaultHandler {
+
+    private StringBuffer charBuffer;
+    private Parameters parameters;
+
+    public ImportXML(String fileName) {
+        // Use the default (non-validating) parser
+        parameters = new Parameters();
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        try {
+            File originalFile = new File(fileName);
+
+            if ((!originalFile.exists()) || (!originalFile.canRead())) {
+                throw new SAXException(
+                        "Parsing Cancelled, file does not exist or is not readable");
+            }
+
+            FileInputStream fis = new FileInputStream(fileName);
+            InputStream finalStream = fis;
+
+            charBuffer = new StringBuffer();
+
+            SAXParser saxParser = factory.newSAXParser();
+            saxParser.parse(finalStream, this);
+
+        } catch (Throwable e) {
+            return;
+        }
+
+    }
+
+    /**
+     * @see org.xml.sax.helpers.DefaultHandler#startElement(java.lang.String,
+     *      java.lang.String, java.lang.String, org.xml.sax.Attributes)
+     */
+    @Override
+    public void startElement(String namespaceURI, String lName, // local name
+            String qName, // qualified name
+            Attributes attrs) throws SAXException {
+    }
+
+    /**
+     * @see org.xml.sax.helpers.DefaultHandler#endElement(java.lang.String,
+     *      java.lang.String, java.lang.String)
+     */
+    @Override
+    public void endElement(String namespaceURI, String sName, // simple name
+            String qName // qualified name
+            ) throws SAXException {
+        if (qName.equals("X")) {
+            try {
+                parameters.x = Integer.valueOf(getTextOfElement());
+            } catch (Exception exception) {
+                parameters.x = 1000;
+            }
+        }
+        if (qName.equals("Y")) {
+            try {
+                parameters.y = Integer.valueOf(getTextOfElement());
+            } catch (Exception exception) {
+                parameters.y = 1000;
+            }
+        }
+
+        if (qName.equals("cicles")) {
+            try {
+                parameters.cicles = Integer.valueOf(getTextOfElement());
+            } catch (Exception exception) {
+                parameters.cicles = 10000;
+            }
+        }
+        if (qName.equals("population")) {
+            try {
+                parameters.population = Integer.valueOf(getTextOfElement());
+            } catch (Exception exception) {
+                parameters.population = 40;
+            }
+        }
+        if (qName.equals("mortality")) {
+            try {
+                parameters.mortality = Integer.valueOf(getTextOfElement());
+            } catch (Exception exception) {
+                parameters.mortality = 40;
+            }
+        }
+        if (qName.equals("patch")) {
+            try {
+                parameters.patch = Integer.valueOf(getTextOfElement());
+            } catch (Exception exception) {
+                parameters.patch = 40;
+            }
+        }
+       
+        if (qName.equals("var1")) {
+            try {
+                parameters.var1 = Integer.valueOf(getTextOfElement());
+            } catch (Exception exception) {
+                parameters.var1 = 4;
+            }
+        }
+        if (qName.equals("var2")) {
+            try {
+                parameters.var2 = Integer.valueOf(getTextOfElement());
+            } catch (Exception exception) {
+                parameters.var2 = 2;
+            }
+        }
+        if (qName.equals("var3")) {
+            try {
+                parameters.var3 = Integer.valueOf(getTextOfElement());
+            } catch (Exception exception) {
+                parameters.var3 = 2;
+            }
+        }
+        if (qName.equals("var4")) {
+            try {
+                parameters.var4 = Integer.valueOf(getTextOfElement());
+            } catch (Exception exception) {
+                parameters.var4 = 2;
+            }
+        }
+
+    }
+
+    public Parameters getParameters() {
+        return parameters;
+    }
+
+    /**
+     * characters()
+     *
+     * @see org.xml.sax.ContentHandler#characters(char[], int, int)
+     */
+    @Override
+    public void characters(char buf[], int offset, int len) throws SAXException {
+        charBuffer = charBuffer.append(buf, offset, len);
+    }
+
+    /**
+     * Return a string without tab an EOF characters
+     *
+     * @return String element text
+     */
+    private String getTextOfElement() {
+        String text = charBuffer.toString();
+        text = text.replaceAll("[\n\r\t]+", "");
+        text = text.replaceAll("^\\s+", "");
+        charBuffer.delete(0, charBuffer.length());
+        return text;
+    }
+}
---
+++ trunk/sinkJava/src/sinkjava/World.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2008-2010 The sinkCell Development Team
+ *
+ * This file is part of sinkCell.
+ *
+ * sinkCell is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * sinkCell is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * sinkCell; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package sinkjava;
+
+import java.util.Random;
+
+/**
+ *
+ * @author sandra
+ */
+public class World {
+
+    private int[][] cellsState;
+    private int[][] proximity;
+    private int[][] habitat;
+    private int x_axis;
+    private int y_axis;
+    private int n_population;
+    private int mortality;
+    private int patch;
+    private Parameters parameters;
+
+    public World(Parameters parameters) {
+
+        this.x_axis = parameters.x;
+        this.y_axis = parameters.y;
+        this.parameters = parameters;
+        this.cellsState = new int[this.x_axis][this.y_axis];        
+        this.proximity = new int[this.x_axis][this.y_axis];
+        this.habitat = new int[this.x_axis][this.y_axis];
+
+        for (int i = 0; i < this.x_axis; i++) {
+            for (int e = 0; e < this.y_axis; e++) {
+                this.cellsState[i][e] = 0;
+                this.proximity[i][e] = 0;
+                this.habitat[i][e] = 0;
+            }
+        }
+
+        this.n_population = parameters.population;
+        this.mortality = parameters.mortality;
+        this.patch = parameters.patch;
+
+        this.init();
+    }
+
+    //Start World's values
+    public void init() {
+        int x = 0, y = 0;
+        Random oRand = new Random();
+        //Init the "habitat" array
+        for (int i = 0; i < this.patch; i++) {
+            x = (oRand.nextInt(this.x_axis - 1) + 1);
+            y = (oRand.nextInt(this.y_axis - 1) + 1);
+
+            for (int j = y - 1; j < y + 2; j++) {
+                for (int e = x - 1; e < x + 2; e++) {
+                    this.habitat[this.normalize(e, this.x_axis)][this.normalize(j, this.y_axis)] = 1;
+                }
+            }
+        }
+
+        //Init the "population" array
+        for (int i = 0; i < this.n_population; i++) {
+            x = (oRand.nextInt(this.x_axis - 1) + 1);
+            y = (oRand.nextInt(this.y_axis - 1) + 1);
+            this.cellsState[x][y] = 1;
+        }
+
+    }
+
+    public int normalize(int coord, int limit) {
+        if (coord < 0) {
+            return (limit - Math.abs(coord)) % limit;
+        } else if (coord >= limit) {
+            return coord - limit;
+        } else {
+            return coord;
+        }
+    }
+
+    //Add the neighbours of every square at "proximity" array
+    public void densityCalculus() {
+        for (int x = 0; x < this.x_axis; x++) {
+            for (int y = 0; y < this.y_axis; y++) {
+                this.proximity[x][y] = 0;
+                for (int j = y - 1; j < y + 2; j++) {
+                    for (int e = x - 1; e < x + 2; e++) {
+                        if (j != y || e != x) {
+                            this.proximity[x][y] += this.cellsState[this.normalize(j, this.x_axis)][this.normalize(e, this.y_axis)];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void update() {
+        int x = 0, y = 0;
+        Random oRand = new Random();
+        for (x = 0; x < this.x_axis; x++) {
+            for (y = 0; y < this.y_axis; y++) {
+                if (this.cellsState[x][y] == 1) {
+                    if ((this.proximity[x][y] >= parameters.var1 && this.habitat[x][y] == 1) || (this.proximity[x][y] >= parameters.var2 && this.habitat[x][y] == 0)) {
+                        this.cellsState[x][y] = 0;
+                    }
+                } else {
+                    if ((this.proximity[x][y] >= parameters.var3 && this.habitat[x][y] == 1) || (this.proximity[x][y] >= parameters.var4 && this.habitat[x][y] == 0)) {
+                        this.cellsState[x][y] = 1;
+                    }
+                }
+            }
+        }
+
+        //Mortality
+        for (int i = 0; i < this.mortality; i++) {
+            x = (oRand.nextInt(this.x_axis - 1) + 1);
+            y = (oRand.nextInt(this.y_axis - 1) + 1);
+            this.cellsState[x][y] = 0;
+        }
+    }
+
+    public void cicle() {
+        this.densityCalculus();
+        this.update();
+    }
+
+    public float getPopulation() {
+        float locaPopulation = 0;
+        for (int x = 0; x < this.x_axis; x++) {
+            for (int y = 0; y < this.y_axis; y++) {
+                locaPopulation += this.cellsState[x][y];
+            }
+        }
+        return locaPopulation;
+    }
+
+    public float getDensity() {
+        float density = this.getPopulation() / (this.x_axis * this.y_axis);
+        return density;
+    }
+
+    public float getProximity() {
+        float localProximity = this.getPopulation() / ((this.x_axis * this.y_axis) / 8);
+        return localProximity;
+    }
+
+    public int[][] getPopulationArray() {
+        return this.cellsState;
+    }
+
+    public int[][] getHabitatArray() {
+        return this.habitat;
+    }
+
+    public Parameters getParameters(){
+        return this.parameters;
+    }
+}
