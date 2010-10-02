4a64e89606cce7550afd43423199e0055c6495b4
nolssi <nolssi@3a5c28fd-5e28-a3e6-50a2-b0e0907970cd>
2010-10-02 16:44:48 +0000

    Graphics work!

--- sinkJava/src/sinkjava/CanvasSink.java
+++ sinkJava/src/sinkjava/CanvasSink.java
@@ -21,6 +21,9 @@ package sinkjava;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
 
 /**
  *
@@ -28,44 +31,88 @@ import java.awt.Graphics;
  */
 public class CanvasSink extends Canvas {
 
-    private int w;
-    private int h;
+    private int canvasSize;
     private World world;
+    private Parameters parameters;
+    Graphics offgc;
+    Image offscreen = null;
 
-    public CanvasSink(World world) {
-        w = 600;
-        h = 600;
+    public CanvasSink(World world, Parameters parameters) {
+        canvasSize = 600;
+        this.parameters = parameters;
         this.world = world;
-        this.setSize(h, w);
+        this.setSize(canvasSize, canvasSize);
         this.setBackground(Color.white);
     }
-    
+
+    /* @Override
+    public Graphics getGraphics(){
+    return offgc;
+    }*/
     @Override
-    public void paint(Graphics g) {
+    public void update(Graphics g) {
+        offscreen = createImage(canvasSize, canvasSize);
+        offgc = offscreen.getGraphics();
+        //offgc = g;
+
         int[][] population = this.world.getPopulationArray();
         int[][] habitat = this.world.getHabitatArray();
 
-        int size = this.world.getParameters().size;
+        int size = parameters.size;
 
         for (int x = 0; x < size; x++) {
             for (int y = 0; y < size; y++) {
-                int n_x = ((this.w * x) / size);
-                int n_y = ((this.h * y) / size);
+                int n_x = ((this.canvasSize * x) / size);
+                int n_y = ((this.canvasSize * y) / size);
                 if (habitat[x][y] == 1) {
-                    g.setColor(Color.black);
-                    g.drawRect(n_x, n_y, this.w / size, this.h / size);
-                    g.setColor(Color.GREEN);
-                    g.fillRect(n_x, n_y, this.w / size, this.h / size);
+                   // offgc.setColor(Color.black);
+                   // offgc.drawRect(n_x, n_y, this.canvasSize / size, this.canvasSize / size);
+                    offgc.setColor(Color.GREEN);
+                    offgc.fillRect(n_x, n_y, this.canvasSize / size, this.canvasSize / size);
                 }
 
                 if (population[x][y] == 1) {
-                    g.setColor(Color.red);
-                    g.fillRect(n_x, n_y, (this.w / size) - 1, (this.h / size) - 1);
+                    offgc.setColor(Color.red);
+                    offgc.fillRect(n_x, n_y, (this.canvasSize / size) - 1, (this.canvasSize / size) - 1);
                 }
             }
-        } 
+        }
+      //  paint(offgc);
+        // transfer offscreen to window
+        g.drawImage(offscreen, 0, 0, this);
+
+        world.cicle();
+
+
     }
-   
+
+    /* @Override
+    public void paint(Graphics g) {
+
+    world.cicle();
+    int[][] population = this.world.getPopulationArray();
+    int[][] habitat = this.world.getHabitatArray();
+
+    int size = parameters.size;
+
+    for (int x = 0; x < size; x++) {
+    for (int y = 0; y < size; y++) {
+    int n_x = ((this.canvasSize * x) / size);
+    int n_y = ((this.canvasSize * y) / size);
+    if (habitat[x][y] == 1) {
+    g.setColor(Color.black);
+    g.drawRect(n_x, n_y, this.canvasSize / size, this.canvasSize / size);
+    g.setColor(Color.GREEN);
+    g.fillRect(n_x, n_y, this.canvasSize / size, this.canvasSize / size);
+    }
+
+    if (population[x][y] == 1) {
+    g.setColor(Color.red);
+    g.fillRect(n_x, n_y, (this.canvasSize / size) - 1, (this.canvasSize / size) - 1);
+    }
+    }
+    }
+    }*/
 }
 
 
