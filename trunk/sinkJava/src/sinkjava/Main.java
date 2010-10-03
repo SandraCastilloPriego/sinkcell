30e9a5cff81d72af301c34f41af047d2c5046e26
nolssi <nolssi@3a5c28fd-5e28-a3e6-50a2-b0e0907970cd>
2010-10-03 10:02:56 +0000

    Exporting simulation to a CSV file

--- trunk/sinkJava/src/sinkjava/Main.java
+++ trunk/sinkJava/src/sinkjava/Main.java
@@ -1,13 +1,27 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2008-2010 The sinkCell Development Team
+ *
+ * This file is part of sinkCell.
+ *
+ * sinkCell is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * sinkCell is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * sinkCell; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 package sinkjava;
 
 /**
  *
- * @author bicha
+ * @author sandra
  */
 public class Main {
 
